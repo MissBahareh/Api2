dd60d912339c41effb3a7929adc367bdc708e76c
Bahareh <shiraz.bpn@gmail.com>
2025-05-06 14:42:33 +0330

    20

--- updateDB.js
+++ updateDB.js
@@ -1,12 +1,10 @@
-const fs = require("fs");
-const fetch = require("node-fetch");
-
 fetch("https://api2-1-6jck.onrender.com/usersLogIN")
   .then((response) => response.json())
   .then((data) => {
     console.log("Data from API:", data);
 
     // ذخیره داده‌ها در `db.json`
+    const fs = require("fs");
     fs.writeFileSync("db.json", JSON.stringify({ usersLogIN: data }, null, 2));
     console.log("db.json updated successfully!");
   })
